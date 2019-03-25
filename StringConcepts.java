62ab3d8076bf534ba7c69bb425561380a43ba00b
Mehta <chhotelal.mehta@capgemini.com>
2019-03-25 16:23:51 +0530

    first commit

---
+++ StringConcepts.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package immutableConcepts;
+
+/**
+ *
+ * @author cmehta
+ */
+public class StringConcepts {
+    
+    
+    public static void main(String[]args){
+        
+        
+        String s1="java";
+        String s2 = "java";
+        
+        System.out.println( " " + s1 +"and " +s2);
+        
+        
+    }
+    
+}
